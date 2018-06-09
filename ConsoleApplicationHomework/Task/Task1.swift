70aec62ed6402eb02b67a01f36423f1c557ea677
Anastasia Derunova <anastasia.derunova@gmail.com>
2018-06-09 13:46:15 +0300

    Added "Task1" and changed organisation's name.

---
+++ ConsoleApplicationHomework/Task/Task1.swift
@@ -0,0 +1,13 @@
+//
+//  Task1.swift
+//  ConsoleApplicationHomework
+//
+//  Created by Anastasia Derunova on 6/9/18.
+//  Copyright © 2018 Anastasia. All rights reserved.
+//
+
+import Foundation
+
+func task1(){
+    print("Task 1")
+}
