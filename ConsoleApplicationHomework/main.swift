4dc6a3701edeb5660710ab379015156914964bdb
Anastasia Derunova <anastasia.derunova@gmail.com>
2018-06-09 13:36:09 +0300

    Added new project.

---
+++ ConsoleApplicationHomework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConsoleApplicationHomework
+//
+//  Created by nastya on 6/9/18.
+//  Copyright © 2018 nastya. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
