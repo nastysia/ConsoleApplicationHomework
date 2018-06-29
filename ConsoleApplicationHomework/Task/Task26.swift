1d98a00c853e0f1ebd260630b1751c9eac9c3cff
Anastasiia Derunova <anastasia.derunova@gmail.com>
2018-06-29 13:30:03 +0300

    Beginning of task 26.

--- ConsoleApplicationHomework/Task/Task26.swift
+++ ConsoleApplicationHomework/Task/Task26.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+func task26 () {
+    print("Введи кількість годин і хивлин.")
+    guard let input = readLine() else {
+        return
+    }
+    let inputnumbers = input.split(separator: " ")
+    guard let hours = Int(inputnumbers[0]), let minutes = Int(inputnumbers[1]), minutes < 60 else {
+        print("А ти точно ввів число годин та хвилин?")
+        return
+    }
+    
+    print("Введи кількість хвилин.")
+    guard let newinput = readLine(), let newminutes = Int(newinput), newminutes < 60 else {
+        print("А ти точно ввів число годин та хвилин?")
+        return
+    }
+    
+    
+    
+    
+    
+}
