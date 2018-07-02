28f6c07a96c70f721e7ce2a2d7e41edd2be84d90
Anastasiia Derunova <anastasia.derunova@gmail.com>
2018-07-02 17:01:20 +0300

    Minor tweaks. Working on task 26.

--- ConsoleApplicationHomework/Task/Task27.swift
+++ ConsoleApplicationHomework/Task/Task27.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 func task27 () {
+    print("Введи кількість годин і хвилин через пробіл.")
+    guard let input = readLine() else {
+        return
+    }
+    let inputnumbers = input.split(separator: " ")
+    guard let tobehours = Int(inputnumbers[0]), let minutes = Int(inputnumbers[1]) else {
+        print("А ти точно ввів число годин та хвилин?")
+        return
+    }
     
+    let ostacha = minutes % 60
+    let addedhours = minutes / 60
+    let hours = tobehours + addedhours
+    
+    
+    print("Добре. А тепер введи кількість хвилин.")
+    guard let newinput = readLine(), let newminutes = Int(newinput) else {
+        print("А ти точно ввів хвилини?")
+        return
+    }
+    
+    let newostacha = newminutes % 60
+    let newhours = newminutes / 60
+    let totalminutes = (ostacha - newostacha) % 60
+    let totalhours = hours - newhours - (totalminutes / 60)
+    
+    
+    print("\(totalhours) h \(totalminutes) min.")
 }
