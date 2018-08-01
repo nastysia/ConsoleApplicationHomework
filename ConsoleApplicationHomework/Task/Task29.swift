ceba91cf3742ba237bafc6ca02db303e413d95ce
Anastasiia Derunova <anastasia.derunova@gmail.com>
2018-08-01 16:00:06 +0300

    Task 29.

--- ConsoleApplicationHomework/Task/Task29.swift
+++ ConsoleApplicationHomework/Task/Task29.swift
@@ -28,11 +28,16 @@ func task29 () {
         return
     }
     
-    let hours = firsthour - secondhour + (firstminutes - secondminutes) / 60
-    let minutes = (firstminutes - secondminutes) % 60
-    
-    
-    print("\(hours) h \(minutes) min.")
-    
+    if (firstminutes - secondminutes) < 0  {
+        let minutes = (firsthour - secondhour - 1) * 60 + (firstminutes - secondminutes)
+        let hours = (firsthour - secondhour) - 1
+        print("\(hours) h \(minutes) min.")
+    }
+        
+    if (firstminutes - secondminutes) > 0  {
+        let minutes = (firstminutes - secondminutes) % 60
+        let hours = firsthour - secondhour + (firstminutes - secondminutes) / 60
+        print("\(hours) h \(minutes) min.")
+    }
 }
 
