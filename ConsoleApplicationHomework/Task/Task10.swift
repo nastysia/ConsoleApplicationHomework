d4c89ef93eb62f1c88da21e6eb2b9da4d0303de5
Anastasia Derunova <anastasia.derunova@gmail.com>
2018-10-28 16:12:15 +0200

    Refactored task 10.

--- ConsoleApplicationHomework/Task/Task10.swift
+++ ConsoleApplicationHomework/Task/Task10.swift
@@ -8,29 +8,26 @@
 
 import Foundation
 
-func task10(){
+func sort2( firstInt a:  inout Int, secondInt b: inout Int) {
+    let maxNumber = max(a, b)
+    let minNumber = min(a, b)
+    a = minNumber
+    b = maxNumber
+}
+
+func task10() {
     print ("Введи два цілі числа через пробіл.")
     guard let input = readLine() else {
         return
     }
     let inputNumbers = input.split(separator:" ")
-    guard let number1 = Int(inputNumbers[0]), let number2 = Int(inputNumbers[1]) else {
+    guard var number1 = Int(inputNumbers[0]), var number2 = Int(inputNumbers[1]) else {
         return
     }
-    // Варіант 1.
-    /*
-     if number1 >= number2 {
-     print(number2, number1)
-     }
-     else if number1 <= number2 {
-     print(number1, number2)
-     }
-     */
-    //Варіант 2.
-    let maxNumber = max(number1, number2)
-    let minNumber = min(number1, number2)
+
+    sort2(firstInt: &number1, secondInt: &number2)
     
-    print (minNumber, maxNumber)
+    print (number1, number2)
     
 }
 
